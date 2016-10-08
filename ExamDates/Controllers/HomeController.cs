9d993e59dbaac32a28fb82bf23f889e1b2f0d45f
KTsolev <ktsolev@gmail.com>
2016-10-08 13:10:30 +0200

    Changed Exam Models
    Added new class for additional dates, added new fields in Exam class, add time to exam dates fields

--- ExamDates/Controllers/HomeController.cs
+++ ExamDates/Controllers/HomeController.cs
@@ -7,18 +7,16 @@ using Microsoft.Office.Interop.Excel;
 using System.Runtime.InteropServices;
 using ExamDates.Models;
 using System.IO;
+using System.Text;
 using System.Web.Routing;
+using System.Threading.Tasks;
+using System.Net;
 
 namespace ExamDates.Controllers
 {
     public class HomeController : Controller
     {
         public List<ExamModel> exams = new List<ExamModel>();
-        // GET: Home
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
         
         public ActionResult Index(string message)
         {
@@ -28,36 +26,55 @@ namespace ExamDates.Controllers
         }
 
         [HttpPost]
-        public ActionResult Result(FormCollection form)
+        public ActionResult Result(String str)
         {
-            string fileName = form["FileName"].ToString();
-            ReadXLSFILE(fileName);
+            ReadXLSFILE(str);
+            exams.OrderBy(c => c.FirstDate).OrderBy(c => c.SecondDate);
             ViewBag.Exams = exams;
             return View();
         }
+
         [HttpPost]
-        public ActionResult Report(FormCollection form)
+        public ActionResult UploadFile(HttpPostedFileBase file)
         {
-            string fileName = form["FileName"].ToString();
-            //check for reportName parameter value now
-            //to do : Return something
-            string error = string.Empty;
+            String fileNew = "";
+            try
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    fileNew = fileName;
+                    var path = Path.Combine(Server.MapPath("~/App_Data/"), fileName);
+                    file.SaveAs(path);
+                }
+                else if (Request.Files.Count > 0)
+                {
+                    file = Request.Files[0];
+                    var fileName = Path.GetFileName(file.FileName);
+                    fileNew = fileName;
+                    var path = Path.Combine(Server.MapPath("~/App_Data/"), fileName);
+                    file.SaveAs(path);
+                    Result(path);
+                }
+
+                if (fileNew != null)
+                {
+                    ViewBag.report = "File uploaded successfully!";
+                    return View("Result");
+                }
+                else 
+                {
+                    ViewBag.report = "File upload failed!";
+                    return View("Index");
+                }
+            }
+            catch (Exception e) 
+            {
+                ViewBag.report = e.Message;
+                return View("Index");
+            }
             
-            if (fileName != null)
-            {
-                ViewBag.FileName = fileName;
-            }
-            else
-            {
-                error = "You have not uploaded file!Please choose excel file!";
-                return (RedirectToAction("Index", new {message = error}));
-            }
-            if (!fileName.EndsWith(".xls") || !fileName.EndsWith(".xlsx"))
-            {
-                error = "You have uploaded wrong file!Please choose excel file!";
-                return (RedirectToAction("Index", new { message = error }));
-            }
-            return View();
+            return View("Result");
         }
         public void ReadXLSFILE(string filename) 
         {
@@ -66,30 +83,76 @@ namespace ExamDates.Controllers
             Microsoft.Office.Interop.Excel.Workbook xlWorkBook = xlApp.Workbooks.Open(filename, 0, true, 5, "", "", true, Microsoft.Office.Interop.Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
             Microsoft.Office.Interop.Excel.Worksheet xlWorkSheet = (Microsoft.Office.Interop.Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
 
-           var range = xlWorkSheet.UsedRange;
+            var range = xlWorkSheet.UsedRange;
 
-            for (rCnt = 2; rCnt <= range.Rows.Count; rCnt++)
-            {
-               string name = (string)(range.Cells[rCnt, 1] as Microsoft.Office.Interop.Excel.Range).Value2;
-               string date1 = (string)(range.Cells[rCnt, 2] as Microsoft.Office.Interop.Excel.Range).Value2;
-               string date2 = (string)(range.Cells[rCnt, 3] as Microsoft.Office.Interop.Excel.Range).Value2;
-               ExamModel exam = new ExamModel();
-               if(name!=null)
-               exam.Name = name;
-               if(date1 != null)
-               exam.FirstDate = DateTime.Parse(date1);
-               if(date2 != null)
-               exam.SecondDate = DateTime.Parse(date2);
-               if(name != null && (date1!=null || date2!=null))
-               exams.Add(exam);
-            }
+           try
+           {
+               for (rCnt = 2; rCnt <= range.Rows.Count; rCnt++)
+               {
+                   string name = (range.Cells[rCnt, 1] as Microsoft.Office.Interop.Excel.Range).Value2;
+                   string date1 = (range.Cells[rCnt, 2] as Microsoft.Office.Interop.Excel.Range).Value2;
+                   string regStartPerDate1 = (range.Cells[rCnt, 3] as Microsoft.Office.Interop.Excel.Range).Value2;
+                   string regEndPerDate1 = (range.Cells[rCnt, 4] as Microsoft.Office.Interop.Excel.Range).Value2;
+                   string date2 = (range.Cells[rCnt, 5] as Microsoft.Office.Interop.Excel.Range).Value2;
+                   string regStartPerDate2 = (range.Cells[rCnt, 6] as Microsoft.Office.Interop.Excel.Range).Value2;
+                   string regEndPerDate2 = (range.Cells[rCnt, 7] as Microsoft.Office.Interop.Excel.Range).Value2;
+                   string session = (range.Cells[rCnt, 8] as Microsoft.Office.Interop.Excel.Range).Value2;
+                   RegistrationDates regDiapason1 = new RegistrationDates();
+                   RegistrationDates regDiapason2 = new RegistrationDates();
+                   ExamModel exam = new ExamModel();
+                   if (name != null)
+                   {
+                       exam.Name = name; 
+                   }
+                   if (date1 != null)
+                   {
+                       exam.FirstDate = DateTime.Parse(date1);
+                   }
+                   if (date2 != null) 
+                   {
+                       exam.SecondDate = DateTime.Parse(date2);
+                   }
+                   if (regStartPerDate1 != null) 
+                   {
+                       regDiapason1.RegStartDate = DateTime.Parse(regStartPerDate1);
+                   }
+                   if (regEndPerDate1 != null) 
+                   {
+                       regDiapason1.RegEndDate = DateTime.Parse(regEndPerDate1);
+                   }
+                   exam.RegPerFirstDate = regDiapason1;
+                   if (regStartPerDate2 != null)
+                   {
+                       regDiapason2.RegStartDate = DateTime.Parse(regStartPerDate2);
+                   }
+                   if (regEndPerDate2 != null)
+                   {
+                       regDiapason2.RegEndDate = DateTime.Parse(regEndPerDate2);
+                   }
+                   exam.RegPerSecondDate = regDiapason2;
+                   if (session != null || session != string.Empty) 
+                   {
+                       exam.Session = session;
+                   }
+                   if (name != null && (date1 != null || date2 != null)) 
+                   {
+                       exams.Add(exam);
+                   }
+               }
+           }
+           catch (Exception e) 
+           {
+               ViewBag.Error = "Something went wrong!";
+           }
+           finally 
+           {
+               xlWorkBook.Close(true, null, null);
+               xlApp.Quit();
 
-            xlWorkBook.Close(true, null, null);
-            xlApp.Quit();
-
-            releaseObject(xlWorkSheet);
-            releaseObject(xlWorkBook);
-            releaseObject(xlApp);
+               releaseObject(xlWorkSheet);
+               releaseObject(xlWorkBook);
+               releaseObject(xlApp);
+           }
         }
          private void releaseObject(object obj)
         {
--- ExamDates/Models/ExamModel.cs
+++ ExamDates/Models/ExamModel.cs
@@ -9,7 +9,16 @@ namespace ExamDates.Models
     {
         public string Name { get; set; }
         public DateTime FirstDate { get; set; }
+        public RegistrationDates RegPerFirstDate { get; set; }
         public DateTime SecondDate { get; set; }
+        public RegistrationDates RegPerSecondDate { get; set; }
         public Boolean Overlap { get; set; }
+        public String Session { get; set; }
+    }
+
+    public class RegistrationDates 
+    {
+        public DateTime RegStartDate { get; set; }
+        public DateTime RegEndDate { get; set; }
     }
 }
